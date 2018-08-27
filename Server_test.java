59af3ee73ffc41186043c96de336630f33218396
Hcsly <42707344+Hcsly@users.noreply.github.com>
2018-08-27 21:11:56 +0800

    Add files via upload

---
+++ Server_test.java
@@ -0,0 +1,15 @@
+package Îå×ÓÆå;
+
+public class Server_test {
+
+	/**
+	 * @param args
+	 */
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Server_frame sf = new Server_frame();
+	}
+
+}
